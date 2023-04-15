fe1bff266e342bf6bc7ef464cad6564fac31248c
Amagi:DDmxh <amagiddmxh@gmail.com>
2023-04-15 23:08:11 +0900

    Fix hook error

--- src/helpers/monitor.ts
+++ src/helpers/monitor.ts
@@ -5,7 +5,7 @@ import useStore from '../store/useStore'
 
 export class monitor  {
   private balanceCron: any
-    
+
   async setBalanceCron (interval: number): Promise<void> {
     clearInterval(this.balanceCron)
     if (interval !== 0) {
@@ -15,7 +15,8 @@ export class monitor  {
 
   async checkBalance (): Promise<void> {
     console.log("start check balance...")
-    //const { selectedAccount, selectedNetwork } = useStore()
+    const { selectedAccount, selectedNetwork } = useStore.getState()
+    console.log("🚀 ~ file: monitor.ts:19 ~ monitor ~ checkBalance ~ selectedAccount:", selectedAccount)
     const provider = new ethers.providers.JsonRpcProvider('https://polygon-mumbai.g.alchemy.com/v2/MD-3rBtr93tbYyDY518rqsBGupOGuvOV')
     const address:any = "0x6C812d6c8dcC8f1a9564E291dB9101Cd273242E5"
     const oldBalance = getFromStorage(address) ?? "0"
@@ -29,9 +30,5 @@ export class monitor  {
       //aa.activateWallet()
     }
     setToLocalStorage(address, newBalance.toString())
-
   }
-
-  
-
 }
